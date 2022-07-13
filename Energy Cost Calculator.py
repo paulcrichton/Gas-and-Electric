719cfb79c93d88d9bdcc12cae757b065d9301217
paulcrichton <78025872+paulcrichton@users.noreply.github.com>
2022-07-13 19:59:35 +0100

    Update

--- Energy Cost Calculator.py
+++ Energy Cost Calculator.py
@@ -1,5 +1,4 @@
 import datetime
-<<<<<<< Updated upstream
 
 
 unit_rate_day = 30.61
@@ -11,28 +10,23 @@ unit_rate_single = 27.8376
 def calculate_cost_E1(unit_rate_day, unit_rate_night):
     last_meter_reading = 166204
     next_meter_reading = 166236
-=======
 import pandas as pd
 import os
 
 def calculate_cost_E1(unit_rate_day, unit_rate_night, next_meter_reading, last_meter_reading):
     """Calculate cost of electric meter 1"""
->>>>>>> Stashed changes
     KWH_used = next_meter_reading - last_meter_reading
     cost = (KWH_used * unit_rate_night)/100
     return cost
 
-<<<<<<< Updated upstream
 def calculate_cost_E2(unit_rate_day, unit_rate_night, unit_rate_single):
     last_meter_reading_night = 299723
     next_meter_reading_night = 299510
     last_meter_reading_day = 84394
     next_meter_reading_day = 85262
 
-=======
 def calculate_cost_E2(unit_rate_day, unit_rate_night, unit_rate_single, next_meter_reading_day, next_meter_reading_night, last_meter_reading_day, last_meter_reading_night):
     """Calculate cost of electric meter 2"""
->>>>>>> Stashed changes
     KWH_used_day = next_meter_reading_day - last_meter_reading_day
     cost_day = (KWH_used_day * unit_rate_day)/100
     
@@ -43,14 +37,11 @@ def calculate_cost_E2(unit_rate_day, unit_rate_night, unit_rate_single, next_met
 
     return cost_day, cost_night, single_rate_cost
 
-<<<<<<< Updated upstream
 def calculate_cost_G1(unit_rate_gas):
     last_meter_reading = int(input("Please Enter Last Gas Reading: "))
     next_meter_reading = int(input("Please Enter Next Gas Reading: "))
-=======
 def calculate_cost_G1(unit_rate_gas, next_meter_reading, last_meter_reading):
     """Caculate cost of gas"""
->>>>>>> Stashed changes
     KWH_used = next_meter_reading - last_meter_reading
     
     cost_gas = (KWH_used * unit_rate_gas)/100
@@ -63,19 +54,14 @@ def total_cost(cost_day, cost_night, single_rate_cost, cost_gas):
     cost_single = single_rate_cost + cost_gas
     return total, cost_single
 
-<<<<<<< Updated upstream
 def record(cost_total, cost_day, cost_night, cost_single, cost_gas):
     date = str(datetime.datetime.now())
-=======
 def cost_record(cost_total, cost_day, cost_night, cost_single, cost_gas):
     """Write monthly cost to cost records"""
     date = datetime.date()
->>>>>>> Stashed changes
     with open("gas_and_electric_cost_record.txt", "a") as file:
         file.write("Total: £{}, Electric Day: £{}, Electric Night: £{}, Cost Single Rate: £{}, Cost Gas: £{}\n".format(cost_total, cost_day, cost_night, cost_single, cost_gas, date))
 
-<<<<<<< Updated upstream
-=======
 def get_last_readings():
     """Get last meter reading for calculation of next months cost"""
     df = pd.read_csv('meter_readings.csv')
@@ -139,12 +125,4 @@ def main():
     print(G1_cost)
 
 
->>>>>>> Stashed changes
-
-cost_gas = calculate_cost_G1(unit_rate_gas)
-cost_day, cost_night, single_rate_cost = calculate_cost_E2(unit_rate_day, unit_rate_night, unit_rate_single)
-
-cost_total, cost_single = total_cost(cost_day, cost_night, single_rate_cost, cost_gas)
-
-print("Total: £{}, Electric Day: £{}, Electric Night: £{}, Cost Single Rate: £{}, Cost Gas: £{}".format(cost_total, cost_day, cost_night, cost_single, cost_gas))
-record(cost_total, cost_day, cost_night, cost_single, cost_gas)
+main()
